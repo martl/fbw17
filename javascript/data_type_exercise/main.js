9376f021a0f6f794030d5d1c0dc2f6e9593fcd25
Martina Freundorfer <martina.freundorfer@digitalcareerinstitute.org>
2019-06-11 11:34:40 +0200

    update of main.js

--- javascript/data_type_exercise/main.js
+++ javascript/data_type_exercise/main.js
@@ -26,3 +26,33 @@ console.log(all);
 console.log(`${name} is ${age} in ${year}`);
 // 7. Declare a variable called isMarried and assign a boolean value. 
 // 8. With your declared variables, print the following "/name/ is married: true/false".
+let isMarried = false;
+console.log(name, "is married:", isMarried); // put different data with comma together 
+// Advantage: data types are not merged to strings with this method 
+// Don't mix with ternary operator: question ? true :false (conditional)
+
+// 9. Declare a variable called programmingLanguage and assign the string "JavaScript". 
+// Declare a variable called isFun and give it a value of true.  
+// Print "JavaScript is fun: true" to the console. 
+let programmingLanguage = "JavaScript";
+let isFun = true; // declared local variable 
+// var isFun = false; // not possible because already used
+// let is always better because it will take care of double assignments
+
+console.log(programmingLanguage + " is Fun: " + isFun);
+
+// 10. Print the J and S of JavaScript from the variable programmingLanguage to the console. 
+// 11. Print the length of the string "JavaScript" to the console.
+
+console.log(programmingLanguage.substring(0,1)); // printing J 
+console.log(programmingLanguage.substring(4,5)); // printing S
+console.log(programmingLanguage.length); // Length property 
+
+// 1. Copy and paste these variables into your JavaScript file.
+//  In the commented section, write down which type you think each variable is. 
+// Check your answers by printing the types to the console.
+
+let length = 16.78; // your answer goes here
+let lastName = "Johnson"; // 
+let car = ""; //
+let isOpen = true; //
