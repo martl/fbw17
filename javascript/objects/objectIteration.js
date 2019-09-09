d3c1813435a75ee18591c056525bf5d84e79cecc
Martina Freundorfer <martina.freundorfer@digitalcareerinstitute.org>
2019-09-09 12:09:02 +0200

    more

--- javascript/objects/objectIteration.js
+++ javascript/objects/objectIteration.js
@@ -14,7 +14,8 @@ var jake = {
     }
 }
 
-for(prop in jake) {
+for (prop in jake) {
+    //keys
     console.log(prop);
 }
 
@@ -24,3 +25,34 @@ console.log(jake.hasOwnProperty('age'));
 
 console.log(jake.hasOwnProperty('x'));
 // False
+
+// https://www.goconqr.com/c/60777/course_modules/90834-calculate-average-skills-exercise?=
+class User {
+    constructor(firstName, lastName, no1, no2, no3) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.skills = {
+            html: no1,
+            css: no2,
+            js: no3
+        };
+    }
+
+    calculateAverage() {
+        let sum = 0;
+        for (let prop in this.skills) { // gives the key
+            sum += this.skills[prop]; // gives the value
+        }
+        let average = sum / 3;
+        return average.toFixed(2);
+    }
+}
+let user1 = new User("Adam", "Lant", 4, 5, 9);
+console.log(user1)
+console.log(user1.calculateAverage());
+let user2 = new User("Lila", "Milo", 9, 8, 3);
+console.log(user2);
+console.log(user2.calculateAverage());
+let user3 = new User("Li", "Mio", 9, 9, 8);
+console.log(user3);
+console.log(user3.calculateAverage());
