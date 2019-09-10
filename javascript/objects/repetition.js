35b7a61db01299c9a79b351328910a07dc003e2f
Martina Freundorfer <martina.freundorfer@digitalcareerinstitute.org>
2019-09-10 13:18:03 +0200

    Repetition

---
+++ javascript/objects/repetition.js
@@ -0,0 +1,28 @@
+// Repetition 
+// Condition with Comparison? if or switch
+if(something === somethingElse) {
+    // do something
+} else {
+    // else case
+}
+
+let i = 0;
+// loop iterating over an array
+while(i < Array.length) {
+    // iterate over an array
+    i++;
+}
+
+// loop for 
+for (let j=0; j< Array.length; j++) {
+    // iterate over the array
+}
+
+// loop for array 
+array.forEach(element => {
+    console.log(element);
+});
+
+// map function returns new array, doesn't change the old one  
+const map1 = array1.map(x => x * 2);
+
