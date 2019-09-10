fb33a8dda0b38d8bda1758509d1d3ffcecd7eb4e
Itamar Givon <itamar.givon.dev@gmail.com>
2019-09-10 13:03:42 +0200

    Revise topics

---
+++ revisions.js
@@ -0,0 +1,62 @@
+let names = [
+  "Leon",
+  "Karol",
+  "Davide",
+  "Martina",
+  "Virgile",
+  "Bianca",
+  "Maria",
+  "Christophe",
+  "Nic",
+  "Hassan",
+  "Mor",
+  "Givara",
+  "Naima",
+  "Balazs",
+  "Ion",
+  "Moj"
+];
+
+// let i = 0;
+
+// While can access arrays
+// while (i < names.length) {
+//   console.log(names[i]);
+//   i++; // i += 1; i = i + 1;
+// }
+
+// // ... but for is better for it
+// for (let i = 0; i < names.length; i++) {
+//   console.log(names[i]);
+// }
+
+// // Aside: function - three ways
+// // Declaration
+// function logMe(element) {
+//   console.log(element);
+// }
+
+// // Expression
+// const logMe = function(element) {
+//   console.log(element);
+// };
+
+// // Arrow function expression
+// const logMe = name => {
+//   console.log(name);
+// };
+
+// // However, for is still too long..
+// // Let's use Array.prototype.forEach()
+// names.forEach((name, i) => {
+//   console.log(`${i}: ${name}`);
+// });
+
+// forEach can be used to... collect and manipulate array values, into a NEW array
+const namesWithNumber = [];
+
+names.forEach((name, i) => {
+  namesWithNumber.push(`${i}: ${name}`);
+});
+
+console.log(namesWithNumber);
