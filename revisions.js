4420054dc5c8754acacb0a9269d0d8670a223002
mojvaf <mvafa44@gmail.com>
2019-09-16 14:26:24 +0200

    new

--- revisions.js
+++ revisions.js
@@ -79,88 +79,78 @@
  * Exercise:
  * Map the array above into a new array containing just the last names
  */
-/*var reformMe = profiles.map(profile => {
-  return profile.lastName
-});
-console.log(reformMe);
-
-const arrayOfStudent = [{
-    name: "Nic",
-    lastName: "Solitom",
-    age: 32
-  },
-  {
-    name: "Naima",
-    lastName: "Adan",
-    age: 28
-  },
-  {
-    name: "Davide",
-    lastName: "De Feudis",
-    age: 30
-  },
-  {
-    name: "Balazs",
-    lastName: "Danyadi",
-    age: 32
-  },
-  {
-    name: "Givara",
-    lastName: "Mahfoud",
-    age: 25
-  },
-  {
-    name: "Mojgan",
-    lastName: "vafa",
-    age: 23
-  },
-  {
-    name: "Lon",
-    lastName: "Lazarev",
-    age: 30
-  },
-  {
-    name: "Maria",
-    lastName: "Nanakou",
-    age: 40
-  },
-  {
-    name: "Karol",
-    lastName: "polakowski",
-    age: 30
-  }
-]
-
-const newArray = arrayOfStudent.map((student) => {
-  return `${student.name} ${student.lastName} is ${student.age} years old.`;
-})
-console.log(newArray);
-*/
 // const profilesLastNames = profiles.map((profile) => {
 //   return `${profile.lastName}`;
 // });
 // console.log(profilesLastNames);
 
-const classMates = [
-  { firstName: "Nic", lastName: "Solitom", age: 32 },
-  { firstName: "Naima", lastName: "Adan Ahmed", age: 29 },
-  { firstName: "Davide", lastName: "de Feudis", age: 35 },
-  { firstName: "Balazs", lastName: "Danyadi", age: 33 },
-  { firstName: "Givara", lastName: "Mahfoud", age: 25 },
-  { firstName: "Maria", lastName: "Nanakou", age: 40 },
-  { firstName: "Mojgan", lastName: "Vafa", age: 23 },
-  { firstName: "Ion", lastName: "Lazarev", age: 30 },
-  { firstName: "Christophe", lastName: "Troudart", age: 23 },
-  { firstName: "Karol", lastName: "Polakowski", age: 30 },
-  { firstName: "Bianca", lastName: "Richa", age: 33 },
-  { firstName: "Hassan", lastName: "Abu Shawish", age: 34 }
+const classMates = [{
+    firstName: "Nic",
+    lastName: "Solitom",
+    age: 32
+  },
+  {
+    firstName: "Naima",
+    lastName: "Adan Ahmed",
+    age: 29
+  },
+  {
+    firstName: "Davide",
+    lastName: "de Feudis",
+    age: 35
+  },
+  {
+    firstName: "Balazs",
+    lastName: "Danyadi",
+    age: 33
+  },
+  {
+    firstName: "Givara",
+    lastName: "Mahfoud",
+    age: 25
+  },
+  {
+    firstName: "Maria",
+    lastName: "Nanakou",
+    age: 40
+  },
+  {
+    firstName: "Mojgan",
+    lastName: "Vafa",
+    age: 23
+  },
+  {
+    firstName: "Ion",
+    lastName: "Lazarev",
+    age: 30
+  },
+  {
+    firstName: "Christophe",
+    lastName: "Troudart",
+    age: 23
+  },
+  {
+    firstName: "Karol",
+    lastName: "Polakowski",
+    age: 30
+  },
+  {
+    firstName: "Bianca",
+    lastName: "Richa",
+    age: 33
+  },
+  {
+    firstName: "hassan",
+    lastName: "Abu Shawish",
+    age: 34
+  }
 ];
 
-// Create a new array of strings. The string should follow the pattern below:
-// Nic Solitom is 32 years old.
-const studentStrings = classMates.map(member => {
-  return `${member.firstName} ${member.lastName} is ${member.age} years old`;
-});
+// // Create a new array of strings. The string should follow the pattern below:
+// // Nic Solitom is 32 years old.
+// const studentStrings = classMates.map(member => {
+//   return `${member.firstName} ${member.lastName} is ${member.age} years old`;
+// });
 
 // console.log(studentStrings);
 
@@ -171,12 +161,24 @@ const studentStrings = classMates.map(member => {
 //   sum += student.age; // sum = sum + student.age
 // });
 
-// console.log(sum); // -> 150
+// console.log(sum); // -> 367
 
 // ...But Array.prototype.reduce is much better for it
 
 // Exercise: Use reduce to do the exact same thing that we did above.
-const sumAges=  classMates.reduce(function (sum, student) {
-         return sum + student.age;
- }, 0) ;
- console.log(sumAges);
+
+const ageSum = classMates.reduce((sum, student) => {
+  return sum + student.age;
+}, 0);
+
+console.log(ageSum); // -> 367
+
+// Exercise: Reduce the classMates array to a boolean that indicates
+// whether there is an object with the firstName Mojgan is in it.
+const theName = classMates.reduce((hasMojgan, studentnames) => {
+  if (studentnames.firstName === "Mojgan") {
+    return studentnames;
+  }
+  return hasMojgan ? true : false
+}, false)
+console.log(theName)
