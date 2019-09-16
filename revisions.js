f1ee7fbfdd2103a5997f72c988c6318f3274f9f7
DCI <you@example.com>
2019-09-16 10:51:36 +0200

    New exercise

--- revisions.js
+++ revisions.js
@@ -1,81 +1,26 @@
-// let names = [
-//   "Leon",
-//   "Karol",
-//   "Davide",
-//   "Martina",
-//   "Virgile",
-//   "Bianca",
-//   "Maria",
-//   "Christophe",
-//   "Nic",
-//   "Hassan",
-//   "Mor",
-//   "Givara",
-//   "Naima",
-//   "Balazs",
-//   "Ion",
-//   "Moj"
-// ];
+//create an array of student objects
 
-// let i = 0;
-
-// While can access arrays
-// while (i < names.length) {
-//   console.log(names[i]);
-//   i++; // i += 1; i = i + 1;
-// }
-
-// // ... but for is better for it
-// for (let i = 0; i < names.length; i++) {
-//   console.log(names[i]);
-// }
-
-// // Aside: function - three ways
-// // Declaration
-// function logMe(element) {
-//   console.log(element);
-// }
-
-// // Expression
-// const logMe = function(element) {
-//   console.log(element);
-// };
-
-// // Arrow function expression
-// const logMe = name => {
-//   console.log(name);
-// };
-
-// // However, for is still too long..
-// // Let's use Array.prototype.forEach()
-// names.forEach((name, i) => {
-//   console.log(`${i}: ${name}`);
-// });
-
-// // forEach can be used to... collect and manipulate array values, into a NEW array
-// const namesWithNumber = [];
-
-// names.forEach((name, i) => {
-//   namesWithNumber.push(`${i}: ${name}`);
-// });
-
-// console.log(namesWithNumber);
-
-// //... BUUUUT, Array.prototype.map is much better for doing it
-// const namesWithNumber = names.map((name, i) => {
-//   return `${i}: ${name}`;
-// });
-
-// console.log(namesWithNumber);
-
-const profiles = [
-  { firstName: "Robert", lastName: "Ristock" },
-  { firstName: "Martina", lastName: "Freundorfer" },
-  { firstName: "Leon-Arno", lastName: "Rimrod" },
-  { firstName: "Itamar", lastName: "Givon" }
+const arrayOfStudents = [
+{firstName:"Maria", lastName:"Nanakou", age:40},
+{firstName:"Mojgan", lastName:"Vafa", age:23},
+{firstName:"Karol", lastName:"Polakowski", age:35},
+{firstName:"Ion", lastName:"Lazarev", age:30},
+{firstName:"Balaz", lastName:"Danyadi", age:30},
+{firstName:"Givara", lastName:"Mahfood", age:25},
+{firstName:"Christophe", lastName:"Chris", age:25},
+{firstName:"Davide", lastName:"deFeudis", age:35},
+{firstName:"Naima", lastName:"AdanAhmed", age:29},
+{firstName:"Nic", lastName:"Solitom", age:32},
 ];
 
-/**
- * Exercise:
- * Map the array above into a new array containing just the last names
- */
+const newArray= arrayOfStudents.map(student => {Solitom", age:32},
+];
+
+const newArray= arrayOfStudents.map(student => {
+return `${student.firstName} ${student.lastName} is ${age} years old`;
+})
+console.log(newArray);
+
+
+
+
