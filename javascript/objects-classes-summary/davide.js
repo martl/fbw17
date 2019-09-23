a47759213f466fc32fc6aad006610c9ea95f7f80
DavideDeFeudis <davidedefeudis@gmail.com>
2019-09-23 12:13:28 +0200

    Delete files

--- javascript/objects-classes-summary/davide.js
+++
@@ -1,6 +0,0 @@
-const isNumberWithinRange = (n, obj) => n >= obj.min && n <= obj.max;
-
-console.log(isNumberWithinRange(4, { min: 0, max: 5 }));
-console.log(isNumberWithinRange(4, { min: 4, max: 5 }));
-console.log(isNumberWithinRange(4, { min: 6, max: 10 }));
-console.log(isNumberWithinRange(5, { min: 5, max: 5 }));
