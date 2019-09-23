105a450e606b26f3df6eb78e575db2dcb683dabe
Nic Solitom <nicsolitom@gmail.com>
2019-09-23 15:15:25 +0200

    Added the second exercise

--- javascript/objects-classes-summary/nic.js
+++ javascript/objects-classes-summary/nic.js
@@ -13,3 +13,17 @@ const checkRange = (number, object) => {
     return number <= object.max && number >= object.min;
 };
 console.log(checkRange(4, { min: 0, max: 5 }));
+
+// Write a function that takes an object and returns a new object with the keys and values in separate arrays.
+// IN: { a:1, b:2, c:3}  OUT: {keys:["a", "b", "c"], values:[1,2,3]}
+// IN: {isBrown: true} OUT: {keys: ["isBrown"], values: [true]}
+
+const splitter = (inputObject) => {
+    return {
+        keys: Object.keys(inputObject),
+        values: Object.values(inputObject)
+    }
+};
+
+console.log(splitter({ a: 1, b: 2, c: 3 }));
+console.log(splitter({ isBrown: true }));
