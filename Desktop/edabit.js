1f65253412052fdc45c1c5ebed943c0afa42854d
Ion Lazarev <vanealazarev@gmail.com>
2019-09-23 16:18:33 +0200

    new

---
+++ Desktop/edabit.js
@@ -0,0 +1,18 @@
+$_$wp(1);
+function checkPalindrome(str) {
+    $_$wf(1);
+    $_$w(1, 0), $_$tracer.log(str.split('').reverse(), 'str.split(\'\').reverse()', 1, 0);
+    $_$w(1, 1), $_$tracer.log(str.split(''), 'str.split(\'\')', 1, 1);
+    return $_$w(1, 2), str.split('') == str.split('').reverse();
+}
+$_$w(1, 3), $_$tracer.log(checkPalindrome('mom'), 'checkPalindrome(\'mom\')', 1, 3);
+$_$w(1, 4), $_$tracer.log([
+    't',
+    'n',
+    't'
+] == [
+    't',
+    'n',
+    't'
+], '[\n    \'t\',\n    \'n\',\n    \'t\'\n] == [\n    \'...', 1, 4);
+$_$wpe(1);
--- javascript/objects-classes-summary/ion_exercise.js
+++ javascript/objects-classes-summary/ion_exercise.js
@@ -3,10 +3,16 @@
 // };
 // console.log(createObject({ a: 1, b: 2, c: 3 })); //{keys:["a","b","c"],values:[1,2,3]}
 
-const tilesSum = arr => {
-  return arr.reduce((total, curr) => {
-    return total + curr.score;
-  },0);
-};
+// const tilesSum = arr => {
+//   return arr.reduce((total, curr) => {
+//     return total + curr.score;
+//   },0);
+// };
 
-console.log(tilesSum([{ tile: "N", score: 1 }, { tile: "O", score: 1 }])); //2
+// console.log(tilesSum([{ tile: "N", score: 1 }, { tile: "O", score: 1 }])); //2
+function isStrangePair(str1, str2) {
+    return str1.endsWith(str2.startsWith()) 
+}
+console.log(isStrangePair("bush", "hubris"))
+tareq="tgfrghthnjyju hgthjyu yjuyjuyu hjyhju"
+console.log(tareq.endsWith(""))
