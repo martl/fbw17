e58fc4deb3feecd0338b367f87a721b3400a7e74
Ion Lazarev <vanealazarev@gmail.com>
2019-09-23 12:17:42 +0200

    exercise 3 update

--- javascript/objects-classes-summary/ion.js
+++ javascript/objects-classes-summary/ion.js
@@ -1,6 +1,6 @@
-const checkRange = function (obj, num) {
+const checkRange = ((obj, num)=> {
     return obj.min <= num && obj.max >= num;
-};
+});
 console.log(checkRange({ min: 0, max: 5 }, 4));
 console.log(checkRange({ min: 4, max: 5 }, 4));
 console.log(checkRange({ min: 6, max: 10 }, 4));
