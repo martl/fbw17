2aea1ac02403cae0b17b2efbae93ff37b600adfa
DavideDeFeudis <davidedefeudis@gmail.com>
2019-09-24 11:42:10 +0200

    Refactor

--- javascript/objects-classes-summary/davide.js
+++ javascript/objects-classes-summary/davide.js
@@ -2,12 +2,8 @@ const helmet = {
     size: 'M',
     price: 17,
     color: 'grey',
-    lock: () => {
-        return 'The helmet is locked';
-    },
-    unlock: () => {
-        return 'The helmet is unlocked';
-    }
+    lock: () => 'The helmet is locked',
+    unlock: () => 'The helmet is unlocked'
 }
 
 console.log(helmet.lock())
