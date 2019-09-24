2a1497eea09659d9f86cf38cc73d086d1fdd9a64
Naima <naima.adanahmed@gmail.com>
2019-09-24 12:27:55 +0200

    erased file exercise

--- javascript/objects-classes-summary/exercises.js
+++
@@ -1,9 +0,0 @@
-const person = {
-  firstName: 'Ali',
-  lastName: 'Abu Zrir',
-  greetings: 'Salam',
-  sayHello: function() {
-    return '$(this.firstName) Says ${this.greeting'}
- }
-
- console.log()
